882eaf82ea7f8207e9ce1d0084f64e2399183303
hobe0m <beom231231@gmail.com>
2023-12-29 15:45:32 +0900

    Setting 작업 1차 완료

--- src/main/java/org/choongang/configs/MvcConfig.java
+++ src/main/java/org/choongang/configs/MvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -31,5 +32,10 @@ public class MvcConfig implements WebMvcConfigurer {
         ms.setBasenames("messages.commons", "messages.validations", "messages.errors");
 
         return ms;
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 }

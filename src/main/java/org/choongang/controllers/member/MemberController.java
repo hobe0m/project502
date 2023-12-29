903421d4686c1f778af8099926c8d85835984cb4
hobe0m <beom231231@gmail.com>
2023-12-29 13:50:26 +0900

    Setting 작업 중(4)

--- src/main/java/org/choongang/controllers/member/MemberController.java
+++ src/main/java/org/choongang/controllers/member/MemberController.java
@@ -1,5 +1,7 @@
 package org.choongang.controllers.member;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.commons.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final Utils utils;
+
     @GetMapping("/join")
     public String join() {
-        return "front/member/join";
+
+
+        return utils.tpl("member/join");
     }
 }

4285fb181e5221f4f2a481c07e14d84cdc8007cd
hobe0m <beom231231@gmail.com>
2024-01-04 10:02:07 +0900

    관리자 페이지 설정(2)

---
+++ src/main/java/org/choongang/admin/config/controllers/BasicConfigController.java
@@ -0,0 +1,33 @@
+package org.choongang.admin.config.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.commons.ExceptionProcessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/config")
+@RequiredArgsConstructor
+public class BasicConfigController implements ExceptionProcessor {
+
+    @ModelAttribute("menuCode")
+    public String getMenuCode() {
+        return "config";
+    }
+
+    @GetMapping
+    public String index(@ModelAttribute BasicConfig config, Model model) {
+        return "admin/config/basic";
+    }
+
+    @PostMapping
+    public String save(BasicConfig config, Model model) {
+        return "admin/config/basic";
+    }
+
+
+}
